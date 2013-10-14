0246380b8321dbc49433961c751144e8a67bb491
Devin Purvis <dgpurv01@louisville.edu>
2013-10-13 20:31:30 -0400

    Updated GeoQuiz from chp 3

--- homework4/GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ homework4/GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
+	
+	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
 	
 	private Button mTrueButton;
 	private Button mFalseButton;
@@ -47,6 +51,7 @@ public class QuizActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
 		
 		mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
@@ -69,16 +74,28 @@ public class QuizActivity extends Activity {
 		
 		mNextButton = (Button)findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				//mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
 				updateQuestion();
 				
 			}
 		});
 		
+		if (savedInstanceState != null) {
+			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+		}
+		
 		updateQuestion();
+		
+		
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		super.onSaveInstanceState(savedInstanceState);
+		Log.i(TAG, "onSaveInstanceState");
+		savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
 	}
 
 	@Override
@@ -86,6 +103,38 @@ public class QuizActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.quiz, menu);
 		return true;
+	
+	
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart() called");
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop() called");
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
 	}
 
 }
