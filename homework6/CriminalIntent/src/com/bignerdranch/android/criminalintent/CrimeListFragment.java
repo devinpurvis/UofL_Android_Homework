4f18ae1633744440eee46f804d57b40a058e0b21
Devin Purvis <dgpurv01@louisville.edu>
2013-10-26 15:42:52 -0400

    overriding getView()

--- homework6/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ homework6/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class CrimeListFragment extends ListFragment {
 	
@@ -30,6 +33,37 @@ public class CrimeListFragment extends ListFragment {
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		Crime c = (Crime)(getListAdapter()).getItem(position);
 		Log.d(TAG, c.getTitle() + " was clicked");
+	}
+	
+	private class CrimeAdapter extends ArrayAdapter<Crime> {
+		public CrimeAdapter(ArrayList<Crime> crimes) {
+			super(getActivity(), 0, crimes);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// If we werent given a view, inflate one
+			if (convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime,
+						null);
+			}
+			
+			//configure the view for this crime
+			Crime c = getItem(position);
+			
+			TextView titleTextView =
+					(TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+			titleTextView.setText(c.getTitle());
+			TextView dateTextView =
+					(TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+			dateTextView.setText(c.getDate().toString());
+			CheckBox solvedCheckBox = 
+					(CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+			solvedCheckBox.setChecked(c.isSolved());
+			
+			return convertView;
+		}
+		
 	}
 
 }
