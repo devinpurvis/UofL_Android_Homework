efe37d97010563eecc581de215b60597e3347158
Devin Purvis <dgpurv01@louisville.edu>
2013-10-26 19:21:00 -0400

    Adding dateOicker to AlertDialog

--- homework6/CriminalIntent/src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ homework6/CriminalIntent/src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -4,11 +4,14 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 
 public class DatePickerFragment extends DialogFragment {
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		return new AlertDialog.Builder(getActivity()).setTitle(R.string.dats_picker_title)
+		View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+		return new AlertDialog.Builder(getActivity()).setView(v)
+				.setTitle(R.string.dats_picker_title)
 				.setPositiveButton(android.R.string.ok, null).create();
 	}
 
