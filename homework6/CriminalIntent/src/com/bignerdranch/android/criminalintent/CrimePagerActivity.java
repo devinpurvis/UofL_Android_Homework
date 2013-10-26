4408b5fb6048ce6098780019c5e4137a62b902db
Devin Purvis <dgpurv01@louisville.edu>
2013-10-26 17:12:01 -0400

    implementing page swipe

--- homework6/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ homework6/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,11 +1,17 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 public class CrimePagerActivity extends FragmentActivity {
 	private ViewPager mViewPager;
+	private ArrayList <Crime> mCrimes;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState ) {
@@ -13,6 +19,23 @@ public class CrimePagerActivity extends FragmentActivity {
 		mViewPager = new ViewPager(this);
 		mViewPager.setId(R.id.viewPager);
 		setContentView(mViewPager);
+		
+		mCrimes = CrimeLab.get(this).getCrimes();
+		
+		FragmentManager fm = getSupportFragmentManager();
+		mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+			@Override
+			public int getCount() {
+				return mCrimes.size();
+			}
+			
+			@Override
+			public Fragment getItem(int pos) {
+				Crime crime = mCrimes.get(pos);
+				return CrimeFragment.newInstance(crime.getId());
+			}
+			
+		});
 		
 	}
 
