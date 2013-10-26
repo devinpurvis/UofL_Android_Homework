12d888e965a4fc9e64c764e56a0a5fb11d5ed200
Devin Purvis <dgpurv01@louisville.edu>
2013-10-26 19:48:33 -0400

    date extraction and DatePicker initialization

--- homework6/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ homework6/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -73,7 +73,7 @@ public class CrimeFragment extends Fragment {
 		mDateButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				FragmentManager fm = getActivity().getSupportFragmentManager();
-				DatePickerFragment dialog = new DatePickerFragment();
+				DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
 				dialog.show(fm, DIALOG_DATE);
 				
 				
