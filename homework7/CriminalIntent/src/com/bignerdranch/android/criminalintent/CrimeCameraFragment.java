ece333ad80ec47ee9d87d9130f7b490436044485
Devin Purvis <dgpurv01@louisville.edu>
2013-11-22 21:09:19 -0500

    Using Camera API

--- homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
+++ homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
@@ -26,6 +26,7 @@ public class CrimeCameraFragment extends Fragment {
 
 
     @Override
+    @SuppressWarnings("deprecation")
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime_camera, parent, false);
 
@@ -39,8 +40,33 @@ public class CrimeCameraFragment extends Fragment {
         });
 
         mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        // deprecated, but required for pre-3.0 devices
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         
         return v; 
+    }
+    
+    @TargetApi(9)
+    @Override
+    public void onResume() {
+        super.onResume();
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {    
+            mCamera = Camera.open(0);
+        } else {
+            mCamera = Camera.open();
+        }
+    }
+    
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
     }
     
 }
