a676a2eba7743193741c6fa30b1892269eb63155
Devin Purvis <dgpurv01@louisville.edu>
2013-11-22 21:25:36 -0500

    end of chapt 19

--- homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeCameraActivity.java
+++ homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeCameraActivity.java
@@ -1,13 +1,22 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CrimeCameraActivity extends SingleFragmentActvity {
-
 	@Override
-	protected Fragment createFragment() {
-		// TODO Auto-generated method stub
-		return new CrimeCameraFragment();
-	}
-
+    public void onCreate(Bundle savedInstanceState) {
+        // hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // hide the status bar and other OS-level chrome
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeCameraFragment();
+    }
 }
--- homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -22,6 +23,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class CrimeFragment extends Fragment {
 	public static final String EXTRA_CRIME_ID = 
@@ -35,6 +37,8 @@ public class CrimeFragment extends Fragment {
 	
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;
+	
+	private ImageButton mPhotoButton;
 	
 	public static CrimeFragment newInstance(UUID crimeId) {
 		Bundle args = new Bundle();
@@ -138,6 +142,22 @@ public class CrimeFragment extends Fragment {
 				mCrime.setSolved(isChecked);
 			}
 		});
+		
+		mPhotoButton = (ImageButton)v.findViewById(R.id.crime_imageButton);
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // launch the camera activity
+                Intent i = new Intent(getActivity(), CrimeCameraActivity.class);
+                startActivity(i);
+            }
+        });
+        
+        // if camera is not available, disable camera functionality
+        PackageManager pm = getActivity().getPackageManager();
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA) &&
+                !pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)) {
+            mPhotoButton.setEnabled(false);
+        }
 		
 		return v;
 	}
