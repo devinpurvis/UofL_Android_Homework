8dde48beb6c523150b7ccec24ca9dc5db77951ec
Devin Purvis <dgpurv01@louisville.edu>
2013-11-22 22:10:49 -0500

    setting and retrieving filename

--- homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ homework7/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -14,6 +14,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,11 +27,13 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class CrimeFragment extends Fragment {
+	private static final String TAG = "CrimeFragment";
 	public static final String EXTRA_CRIME_ID = 
 			"com.bignerdranch.android.criminalintent.crime_id";
 	
 	private static final String DIALOG_DATE = "date";
 	private static final int REQUEST_DATE = 0;
+	private static final int REQUEST_PHOTO = 1;
 	
 	private Crime mCrime;
 	private EditText mTitleField;
@@ -57,6 +60,13 @@ public class CrimeFragment extends Fragment {
 			Date date = (Date)data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
 			mCrime.setDate(date);
 			updateDate();
+		} else if (requestCode == REQUEST_PHOTO) {
+            // create a new Photo object and attach it to the crime
+            String filename = data
+                .getStringExtra(CrimeCameraFragment.EXTRA_PHOTO_FILENAME);
+            if (filename != null) {
+                Log.i(TAG, "filename: " + filename);
+            }
 		}
 	}
 	
@@ -148,7 +158,7 @@ public class CrimeFragment extends Fragment {
             public void onClick(View v) {
                 // launch the camera activity
                 Intent i = new Intent(getActivity(), CrimeCameraActivity.class);
-                startActivity(i);
+                startActivityForResult(i, REQUEST_PHOTO);
             }
         });
         
